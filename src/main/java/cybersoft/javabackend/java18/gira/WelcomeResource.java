b92d13f37569ce9b02d9872c99f0d0c2ea25c9a3
duongtangtai <107338278+duongtangtai@users.noreply.github.com>
2022-09-18 15:46:38 +0700

    feat: add BaseEntity with JPA Auditor, CJB-3

--- src/main/java/cybersoft/javabackend/java18/gira/WelcomeResource.java
+++ src/main/java/cybersoft/javabackend/java18/gira/WelcomeResource.java
@@ -1,10 +1,9 @@
-package cybersoft.javabackend.java18.gira.resources;
+package cybersoft.javabackend.java18.gira;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class WelcomeResource {
